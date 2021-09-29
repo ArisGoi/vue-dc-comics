12d79d51e0788e61985cc445063763b74d69932b
ArisGoi <aris.goi97@gmail.com>
2021-09-29 17:26:28 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <Header/>
 
     <main>
-      
+
     </main>
 
     <Footer/>
@@ -30,11 +30,6 @@ export default {
 @import '@/assets/style/style.scss';
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: $header-height;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,22 @@
 <template>
   <header>
-    <h1>Header pagina</h1>
+    <div class="container flex-ori">
+      <img class="logo" src="@/assets/img/dc-logo.png" alt="Logo DC">
+      <nav>
+        <ul>
+          <li><a href="#">Characters</a></li>
+          <li><a href="#">Comics</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">TV</a></li>
+          <li class="current"><a href="#">Games</a></li>
+          <li><a href="#">Collectibles</a></li>
+          <li><a href="#">Videos</a></li>
+          <li><a href="#">Fans</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Shop</a></li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
@@ -18,9 +34,49 @@ export default {
     position: fixed;
     top: 0;
     left: 0;
-    height: 5rem;
     width: 100%;
-    background-color: pink;
-    display: flex;
+    background-color: $c-header;
+
+    .container{
+      justify-content: space-between;
+      align-items: center;
+      height: $header-height;
+    }
+    .logo{
+      height: $header-logo-h;
+    }
+
+    nav ul{
+      display: flex;
+
+      li{
+        height: $header-height;
+        display: flex;
+        align-items: center;
+        position: relative;
+
+        &.current a{
+          color: $c-first;
+        }
+        &.current::after{
+          content: '';
+          position: absolute;
+          bottom: 0;
+          left: 0;
+
+          height: 5px;
+          width: 100%;
+          background-color: $c-first;
+        }
+      }
+
+      a{
+        color: $c-text-main;
+        text-decoration: none;
+        font-weight: 700;
+        padding: 15px;
+        font-size: 18px;
+      }
+    }
   }
 </style>
