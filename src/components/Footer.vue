985bb1fda9dc3593aa07d92113466a9018cd6447
ArisGoi <aris.goi97@gmail.com>
2021-09-29 16:39:55 +0200

    add struttura iniziale

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1>Footer pagina</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>Header pagina</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  @import '@/assets/style/style.scss';
+
+  header{
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 5rem;
+    width: 100%;
+    background-color: pink;
+    display: flex;
+  }
+</style>
